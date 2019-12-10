a4304365a5dfe0b1eb953368fd82800c9daf6fb6
sabelogumede <iamsabelogumede@gmail.com>
2019-12-10 12:18:18 +0200

    JWT auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,7 @@ export const newToken = user => {
     expiresIn: config.secrets.jwtExp
   })
 }
-
+// veryfy token
 export const verifyToken = token =>
   new Promise((resolve, reject) => {
     jwt.verify(token, config.secrets.jwt, (err, payload) => {
@@ -16,10 +16,65 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+// signup
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'Email and password required' })
+  }
+  try {
+    const user = await User.create(req.body)
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(400).end()
+  }
+}
 
-export const signin = async (req, res) => {}
+// signin
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'Email and password required' })
+  }
 
+  const user = await User.findOne({ email: req.body.email }).exec()
+  if (!user) {
+    return res.status(401).send({ message: 'Not authorised' })
+  }
+
+  try {
+    const match = await user.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'Not authorised' })
+    }
+    const token = newToken(user)
+    return res.status(201).send({ token })
+  } catch (e) {
+    console.error(e)
+    return res.status(401).send({ message: 'Not authorised' })
+  }
+}
+
+// protect routes with JWT
 export const protect = async (req, res, next) => {
-  next()
+  if (!req.headers.authorization) {
+    return res.status(401).end()
+  }
+  let token = req.headers.authorization.split('Bearer ')[1]
+
+  if (!token) {
+    return res.status(401).end()
+  }
+
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+  } catch (e) {
+    console.error(e)
+    return res.status(401).end()
+  }
 }
